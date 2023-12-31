54a25cfa8fd1ffa4b99f3710859f69cf621c75bb
Se7enSe7enSe7en <50391562+Se7enSe7enSe7en@users.noreply.github.com>
2023-12-31 23:17:57 +0800

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://se7ense7ense7en.github.io/new-year-countdown-vue/",
+  base: "new-year-countdown-vue",
   plugins: [vue()],
   resolve: {
     alias: {
